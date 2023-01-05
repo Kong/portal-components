1cc25123e49e3743ccf972014487e3c5ebd040d5
kai.arrowood <kai.arrowood@konghq.com>
2023-01-05 12:12:07 -0500

    fix(*): pr feedback

--- packages/vue-spec-renderer/src/components/VueSpecRenderer.vue
+++ packages/vue-spec-renderer/src/components/VueSpecRenderer.vue
@@ -17,3 +17,10 @@ defineProps({
 })
 </script>
 
+<style lang="scss" scoped>
+.vue-spec-renderer {
+  font-family: var(--kong-portal-vue-spec-renderer--font-family-default, Roboto, Helvetica, sans-serif);
+  font-size: var(--kong-portal-vue-spec-renderer--font-size, 16px);
+  color: var(--kong-portal-vue-spec-renderer--text-color, #000);
+}
+</style>
