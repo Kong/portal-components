0d57b596e09bdaf63724718cd52757c4e68b7ef8
kai.arrowood <kai.arrowood@konghq.com>
2023-01-10 16:08:14 -0500

    feat(vue-spec-renderer): add prop support

--- packages/vue-spec-renderer/src/components/VueSpecRenderer.vue
+++ packages/vue-spec-renderer/src/components/VueSpecRenderer.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="vue-spec-renderer">
-    <kong-swagger-ui :spec="document"></kong-swagger-ui>
+  <div class="kong-portal-vue-spec-renderer">
+    <kong-swagger-ui v-if="document" :spec="document" :has-sidebar="hasSidebar" :essentials-only="essentialsOnly"></kong-swagger-ui>
+    <div v-else>Error: No document spec provided</div>
   </div>
 </template>
 
@@ -9,16 +10,26 @@ import '@kong/swagger-ui-web-component'
 import { PropType } from 'vue'
 import { Document } from '../types'
 
+// TODO: add prop for url
+
 defineProps({
   document: {
     type: Object as PropType<Document>,
     required: true
+  },
+  hasSidebar: {
+    type: Boolean,
+    default: true
+  },
+  essentialsOnly: {
+    type: Boolean,
+    default: false
   }
 })
 </script>
 
 <style lang="scss" scoped>
-.vue-spec-renderer {
+.kong-portal-vue-spec-renderer {
   font-family: var(--kong-portal-vue-spec-renderer-font-family-default, Roboto, Helvetica, sans-serif);
   font-size: var(--kong-portal-vue-spec-renderer-font-size, 16px);
   color: var(--kong-portal-vue-spec-renderer-text-color, #000);
